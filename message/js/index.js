e3558d8722113d124758d6d24f979abad142e053
gyan-droid <68509384+gyan-droid@users.noreply.github.com>
2025-12-31 22:48:46 +0530

    Update message text for New Year greeting

--- message/js/index.js
+++ message/js/index.js
@@ -1,6 +1,6 @@
 this.$dom = {};
 this.$dom.animatedText = document.createElement('div');
-this.text = "THIS IS MESSAGE SECTION OF THIS WEBSITE.DEMO MESSAGE- Your custom message";  // MESSAGE AREA , THIS CAN BE CHANGED FROM YOUR MESSAGE :)
+this.text = "I’m ending 2025 with you and 2026 is also beginning with you and that alone makes this moment special for me I wish this coming year brings you happiness peace and everything your heart desires truly everything you deserve it all I can’t really put into words how much you mean to me how much your happiness matters to me how much even your smallest little things matter I don’t know what the future holds whether we meet again in this life or in another or if there’s even something like rebirth but I know this for sure some part of you will always live in my heart there is no place in this world no people no friends that give me the peace I feel when I’m with you the sukoon I get exists only with you you make me a better man with you everything feels better lighter and more meaningful you are the most positive and beautiful influence in my life I feel this page would fall short if I started writing everything I feel for you happy new year I wish you happiness for this year and for all the years to come and with all my heart I wish you my happiness too";  // MESSAGE AREA , THIS CAN BE CHANGED FROM YOUR MESSAGE :)
 this.$dom.container = document.getElementById('container');
 this.$dom.container.appendChild(this.$dom.animatedText);
 animateText();
